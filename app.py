9cf1d0f0e04244add72bd277691cfbe64278ca0a
Bramdv <bramdevroey@gmail.com>
2021-05-04 11:40:07 +0200

    completed index page

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
+
+from flask.json import jsonify
 
 app = Flask(__name__)
 
@@ -11,3 +13,20 @@ with open("books/books.json", "r") as jsonfile:
 @app.route("/")
 def index():
     return render_template("index.html", books=available_books)
+
+
+@app.route("/summarize", methods=["GET", "POST"])
+def summarize():
+    if request.method == "POST":
+        # user sent raw text
+        return request.form.get("text")
+
+    elif request.args.get("book"):
+        # use selected predownloaded book
+        return jsonify(available_books[int(request.args.get("book"), 0)])
+
+    elif request.args.get("url"):
+        # user sent link
+        return "Link loading not implemented."
+
+    return "Not ready"
